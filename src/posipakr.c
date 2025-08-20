da253a21b736b6ea603bc04f124c51e842781cf9
stx3plus1 <stx4@ballpa.in>
2025-08-20 13:18:50 +0200

    posipakr: relocate macros, fix up argument parsing

--- src/posipakr.c
+++ src/posipakr.c
@@ -9,6 +9,24 @@
 
 #include "posipakx.h"
 
+#define destroy_quit(exit) destroy_posipak(pak); return exit;
+#define bad_pak_helper(pakf) \
+if (memcmp(pakf->validation, file_signature, 4)) { \
+	puts("[x] bad file found in pak :<"); \
+	continue; \
+}
+#define arg_count(count) \
+if (options.file_amt < count) { \
+	puts("[x] not enough arguments >:/"); \
+	print_help(); \
+	destroy_quit(1); \
+} \
+else if (options.file_amt >= 1 && !f[0] || options.file_amt >= 2 && !f[1] || options.file_amt >= 3 && !f[2]) { \
+	puts("[x] missing some file args!?"); \
+	print_help(); \
+	destroy_quit(1); \
+}
+
 typedef struct {
 	int add;
 	int list;
@@ -18,7 +36,7 @@ typedef struct {
 	int extract;
 	int file_amt;
 } pakr_options;
-char *f1, *f2, *f3;
+char* f[3] = {0};
 
 const char* help[] = {
 	"",
@@ -32,6 +50,7 @@ const char* help[] = {
 	"	-a: add file     (pak, filename, pakname)",
 	"	-d: dump file    (pak, filename, pakname)",
 	"\n"
+	"don't even try mix same-count argument cmds",
 	"(back up your pak, unless you like chaos)",
 	"",
 	"returning you to your freedom..."
@@ -42,24 +61,6 @@ void print_help(void) {
 		puts(help[i]);
 }
 
-#define destroy_quit(exit) destroy_posipak(pak); return exit;
-#define bad_pak_helper(pakf) \
-if (memcmp(pakf->validation, file_signature, 4)) { \
-	puts("[x] bad file found in pak :<"); \
-	continue; \
-}
-#define arg_count(count) \
-if (opt.file_amt != count) { \
-	puts("[x] incorrect number of file args >:/"); \
-	print_help(); \
-	destroy_quit(1); \
-} \
-else if (opt.file_amt >= 1 && !f1 || opt.file_amt >= 2 && !f2 || opt.file_amt >= 3 && !f3) { \
-	puts("[x] missing some file args!?"); \
-	print_help(); \
-	destroy_quit(1); \
-}
-
 int main(int ac, char* av[]) {
 	if (ac < 2) {
 		puts("[i] no options provided");
@@ -67,57 +68,53 @@ int main(int ac, char* av[]) {
 		return 0;
 	}
 
-	pakr_options opt = { 0 };
+	pakr_options options = { 0 };
+	int j = 0;
 	for (int i = 1; i < ac; i++) {
 		if (av[i][0] == '-') {
 			for (char* p = (av[i] + 1); *p; p++) {
 				switch (*p) {
-				case 'c': opt.create  = 1; break;
-				case 'l': opt.list    = 1; break;
-				case 'e': opt.extract = 1; break;
-				case 'r': opt.delete  = 1; break;
-				case 'a': opt.add     = 1; break;
-				case 'd': opt.dump    = 1; break;
+				case 'c': options.create  = 1; break;
+				case 'l': options.list    = 1; break;
+				case 'e': options.extract = 1; break;
+				case 'r': options.delete  = 1; break;
+				case 'a': options.add     = 1; break;
+				case 'd': options.dump    = 1; break;
 
 				default: printf("[i] unknown option: %c\n", *p); break;
 				}
 			}
 		} else {
-			if      (!f1 && !f2 && !f3) f1 = av[i], opt.file_amt = 1;
-			else if ( f1 && !f2 && !f3) f2 = av[i], opt.file_amt = 2;
-			else if ( f1 &&  f2 && !f3) f3 = av[i], opt.file_amt = 3;
-			else {
-				printf("[x] additional extra argument at position %d :/", i);
-				return 1;
-			}
+			if (j < 3) f[j++] = av[i], options.file_amt = j + 1;
+			else printf("[x] ignoring unusable argument \"%s\"\n", av[i]);
 		}
 	}
 
-	if (opt.file_amt < 1) {
+	if (options.file_amt < 1) {
 		puts("[x] perform operations on... what? provide a file >:(");
 		print_help();
 		return 1;
 	}
 
 	posipak* pak = NULL;
-	if (opt.create) {
+	if (options.create) {
 		pak = create_posipak(); 
 		puts("[i] created new pak");
 	} else {
-		pak = load_posipak(f1);
+		pak = load_posipak(f[0]);
 		if (!pak) {
 			puts("[x] invalid file or pak signature :c");
 			return 1;
 		}
 	}
 
-	if (opt.add) {
+	if (options.add) {
 		arg_count(3);
 
 		long file_len;
 		FILE* load_file;
 
-		load_file = fopen(f2, "rb");
+		load_file = fopen(f[1], "rb");
 		if (!load_file) {
 			puts("[x] failed to open file (does it exist?) :c");
 			destroy_quit(1);
@@ -142,21 +139,21 @@ int main(int ac, char* av[]) {
 		}
 		fclose(load_file);
 
-		pak_add_file(pak, f3, file_data, file_len);
+		pak_add_file(pak, f[2], file_data, file_len);
 		free(file_data);
-		printf("[i] added %s as %s to pak\n", f2, f3);
+		printf("[i] added %s as %s to pak\n", f[1], f[2]);
 	}
 
-	if (opt.delete) {
-		int ret = pak_delete_file(pak, f2);
+	if (options.delete) {
+		int ret = pak_delete_file(pak, f[1]);
 		if (ret) {
 			puts("[x] failed to delete file (does it exist in-pak?) :c");
 			destroy_quit(1);
 		}
-		printf("[i] deleted %s from pak\n", f2);
+		printf("[i] deleted %s from pak\n", f[1]);
 	}
 
-	if (opt.list) {
+	if (options.list) {
 		if (pak->header.lookup_len == 0) {
 			puts("[i] there's nothing in there");
 		} else {
@@ -169,19 +166,19 @@ int main(int ac, char* av[]) {
 		}
 	}
 	
-	if (opt.dump) {
+	if (options.dump) {
 		arg_count(3);
 
 		long file_len;
 		FILE* dump_file;
 		char* file_data = NULL;
 
-		dump_file = fopen(f2, "wb");
+		dump_file = fopen(f[1], "wb");
 		if (!dump_file) {
 			puts("[x] failed to open output file");
 		}
 
-		file_data = pak_load_file(pak, f3, &file_len);
+		file_data = pak_load_file(pak, f[2], &file_len);
 		if (!file_data) {
 			puts("[x] failed to read pak file data (does it exist in-pak?) :c");
 			destroy_quit(1);
@@ -196,10 +193,10 @@ int main(int ac, char* av[]) {
 		fclose(dump_file);
 		free(file_data);
 
-		printf("[i] dumped %s to %s\n", f3, f2);
+		printf("[i] dumped %s to %s\n", f[2], f[1]);
 	}
 	
-	if (opt.extract) {
+	if (options.extract) {
 		long file_len;
 		FILE* dump_file;
 		char* file_data = NULL;
@@ -228,7 +225,7 @@ int main(int ac, char* av[]) {
 		}
 	}
 
-	if (opt.create || opt.add || opt.delete) write_posipak(pak, f1);
+	if (options.create || options.add || options.delete) write_posipak(pak, f[0]);
 	if (pak) destroy_posipak(pak);
 	return 0;
 }
